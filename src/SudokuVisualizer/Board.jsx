2a17888d8d1615c93589a9b1c872b1480b42c0a8
Mahdi Robbani <mahdi.robbani@gmail.com>
2021-10-20 16:59:36 +0200

    created board

--- src/SudokuVisualizer/Board.jsx
+++ src/SudokuVisualizer/Board.jsx
@@ -3,31 +3,58 @@ import Tile from './Tile';
 import './Board.css';
 
 export default class Board extends Component {
-    renderTile(i){
-        return <Tile value={i}/>;
+    constructor(props){
+        super(props);
+        this.numRows = 9;
+        this.numCols = 9;
+        this.state = {
+            values: [[null, null, null, 2, 6, null, 7, null, 1],
+                     [6, 8, null, null, 7, null, null, 9, null],
+                     [1, 9, null, null, null, 4, 5, null, null],
+                     [8, 2, null, 1, null, null, null, 4, null],
+                     [null, null, 4, 6, null, 2, 9, null, null],
+                     [null, 5, null, null, null, 3, null, 2, 8],
+                     [null, null, 9, 3, null, null, null, 7, 4],
+                     [null, 4, null, null, 5, null, null, 3, 6],
+                     [7, null, 3, null, 1, 8, null, null, null]],
+        }
     }
+
+    handleClick(i, j) {
+        // const squares = this.state.squares.slice();
+        // squares[i] = 'X';
+        // this.setState(squares);
+        console.log(i, j)
+    }
+
+    renderTile(i, j){
+        return <Tile 
+                    value={this.state.values[i][j]}
+                    onClick={() => this.handleClick(i, j)}
+                />;
+    }
+
 
     render() {
         const status = 'Next player: X';
+        
+        let rows = []
+        for (let i = 0; i < this.numRows; i++){
+            let cols = [];
+            for (let j = 0; j < this.numCols; j++){
+                cols.push(<React.Fragment>
+                    {this.renderTile(i,j)}
+                </React.Fragment>)
+            }
+            rows.push(<div className="board-row" key={i}>
+                {cols}
+            </div>)
+        }
+        console.log(rows)
 
         return (
             <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderTile(0)}
-                    {this.renderTile(1)}
-                    {this.renderTile(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderTile(3)}
-                    {this.renderTile(4)}
-                    {this.renderTile(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderTile(6)}
-                    {this.renderTile(7)}
-                    {this.renderTile(8)}
-                </div>
+                {rows}
             </div>
 
         );
--- src/SudokuVisualizer/Tile.jsx
+++ src/SudokuVisualizer/Tile.jsx
@@ -2,16 +2,13 @@ import React, {Component} from 'react';
 import './Tile.css';
 
 export default class Tile extends Component{
-    // constructor(props){
-    //     super(props);
-    //     this.state={
-    //         value: null,
-    //     };
-    // }
+
 
     render() {
         return (
-            <button className="tile">
+            <button className="tile"
+                    onClick={() => this.props.onClick()}
+                >
                 {this.props.value}
                 </button>
         );
