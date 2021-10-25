75d83e9eb1e196a295c128e7579e649d7573a4a4
Mahdi Robbani <mahdi.robbani@gmail.com>
2021-10-25 16:02:55 +0200

    Added extra tile information

--- src/SudokuVisualizer/Board.jsx
+++ src/SudokuVisualizer/Board.jsx
@@ -3,22 +3,57 @@ import Tile from './Tile';
 import './Board.css';
 import {backtrack} from '../algorithms/backtrack';
 
+const INITIAL_BOARD = [[0, 0, 0, 2, 6, 0, 7, 0, 1],
+                        [6, 8, 0, 0, 7, 0, 0, 9, 0],
+                        [1, 9, 0, 0, 0, 4, 5, 0, 0],
+                        [8, 2, 0, 1, 0, 0, 0, 4, 0],
+                        [0, 0, 4, 6, 0, 2, 9, 0, 0],
+                        [0, 5, 0, 0, 0, 3, 0, 2, 8],
+                        [0, 0, 9, 3, 0, 0, 0, 7, 4],
+                        [0, 4, 0, 0, 5, 0, 0, 3, 6],
+                        [7, 0, 3, 0, 1, 8, 0, 0, 0]]
+// [null, null, null, 2, 6, null, 7, null, 1],
+// [6, 8, null, null, 7, null, null, 9, null],
+// [1, 9, null, null, null, 4, 5, null, null],
+// [8, 2, null, 1, null, null, null, 4, null],
+// [null, null, 4, 6, null, 2, 9, null, null],
+// [null, 5, null, null, null, 3, null, 2, 8],
+// [null, null, 9, 3, null, null, null, 7, 4],
+// [null, 4, null, null, 5, null, null, 3, 6],
+// [7, null, 3, null, 1, 8, null, null, null]
+
 export default class Board extends Component {
     constructor(props){
         super(props);
         this.numRows = 9;
         this.numCols = 9;
         this.state = {
-            grid: [[null, null, null, 2, 6, null, 7, null, 1],
-                     [6, 8, null, null, 7, null, null, 9, null],
-                     [1, 9, null, null, null, 4, 5, null, null],
-                     [8, 2, null, 1, null, null, null, 4, null],
-                     [null, null, 4, 6, null, 2, 9, null, null],
-                     [null, 5, null, null, null, 3, null, 2, 8],
-                     [null, null, 9, 3, null, null, null, 7, 4],
-                     [null, 4, null, null, 5, null, null, 3, 6],
-                     [7, null, 3, null, 1, 8, null, null, null]],
+            grid: [],
         }
+    }
+
+    getInitialGrid(){
+        let grid = [];
+        for (let i = 0; i < this.numRows; i++){
+            let currentRow = [];
+            for (let j = 0; j < this.numCols; j++){
+                const value = INITIAL_BOARD[i][j] ? INITIAL_BOARD[i][j] : null;
+                const row = i;
+                const col = j;
+                const isBorder = row % 3 === 2 ? true: false;
+                const isInitial = value ? true: false;
+                const tile = {row, col, value, isBorder, isInitial};
+                currentRow.push(tile);
+            }
+            grid.push(currentRow);
+        }
+        return grid
+    }
+
+    componentDidMount(){
+        // set initial state of board
+        const grid = this.getInitialGrid();
+        this.setState({grid})
     }
 
     handleClick(i, j) {
@@ -64,35 +99,41 @@ export default class Board extends Component {
         }
     }
 
-    renderTile(i, j){
-        return <Tile 
-                    value={this.state.grid[i][j]}
-                    onClick={() => this.handleClick(i, j)}
+    renderTile(tile){
+        const {row, col, value, isBorder, isInitial} = tile;
+        return <Tile
+                    key={`${row},${col}`}
+                    col={col}
+                    value={value}
+                    isBorder={isBorder}
+                    isInitial={isInitial}
+                    onClick={() => this.handleClick(row, col)}
                 />;
     }
 
-
     render() {
-        const status = 'Next player: X';
+        const {grid} = this.state;
         
-        let rows = []
-        for (let i = 0; i < this.numRows; i++){
-            let cols = [];
-            for (let j = 0; j < this.numCols; j++){
-                cols.push(<React.Fragment>
-                    {this.renderTile(i,j)}
+        let board = [];
+        grid.forEach((row, rowIdx) => {
+            let currentRow = [];
+            row.forEach((tileData, tileDataIdx) => {
+                currentRow.push(<React.Fragment key={tileDataIdx}>
+                    {this.renderTile(tileData)}
                 </React.Fragment>)
-            }
-            rows.push(<div className="board-row" key={i}>
-                {cols}
-            </div>)
-        }
-        //console.log(rows)
+            })
+            board.push(<div className="board-row" key={rowIdx}>
+                    {currentRow}
+                </div>)
+        })
+
 
         return (
             <div>
                 <button onClick={() => this.visualizeBacktrack()}>Solve Board</button>
-                {rows}
+                <div className="grid">
+                    {board}
+                </div>
             </div>
 
         );
